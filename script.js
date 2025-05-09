3f121a88edd2dc9e1857ddeb80cca207c8c4c86a
Mahesh-2007 <mm2472229@gmail.com>
2025-05-09 20:50:37 +0530

    js added

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('input[name="display"]');
+const buttons = document.querySelectorAll('.btn'); 
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.value;
+
+        if (value === 'AC') {
+            display.value = '';
+        } else if (value === 'DE') {
+            display.value = display.value.slice(0, -1);
+        } else if (value === '=') {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = 'Error';
+            }
+        } else if (value === 'x') {
+            display.value += '*';
+        } else {
+            display.value += value;
+        }
+    });
+});
